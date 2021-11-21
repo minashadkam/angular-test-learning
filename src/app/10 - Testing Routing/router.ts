b47fbcce996d7e3ed979004cfac6334d647c4277
m.shadkam <m.shadkam@sadad.co.ir>
2021-11-21 10:41:10 +0330

    testing  router.ts

--- src/app/10 - Testing Routing/router.ts
+++ src/app/10 - Testing Routing/router.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
-import { Routes } from "@angular/router";
+import {Component, NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   template: `Search`
@@ -7,20 +9,30 @@ import { Routes } from "@angular/router";
 export class SearchComponent {
 }
 
+const searchRoute = [{path: '', component: SearchComponent}];
+
+@NgModule({
+  imports: [RouterModule.forChild(searchRoute)],
+})
+export class SearchModule{}
+
 @Component({
   template: `Home`
 })
 export class HomeComponent {
 }
 
-@Component({
-  template: `<router-outlet></router-outlet>`
-})
-export class AppComponent {
-}
 
-export const routes: Routes = [
+export const AppRoute: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'search', component: SearchComponent }
+  {path: 'home', component: HomeComponent},
+  {path: 'search', loadChildren: () => SearchModule},
 ];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(AppRoute)],
+  declarations: [],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
